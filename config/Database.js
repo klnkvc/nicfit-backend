ee74a3196918c97516738d51cc9a2b30bfe8c70b
izanmaizan <izanmaizan@gmail.com>
2024-06-24 20:22:10 +0700

    update

--- config/Database.js
+++ config/Database.js
@@ -4,10 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: "buknqdmsueylkwqwuone-mysql.services.clever-cloud.com",
-  user: "us0dy77rie8hlvgu",
-  password: "8yY49LvERMunYDOEbvsL",
-  database: "buknqdmsueylkwqwuone",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
 });
 
 export default pool;
